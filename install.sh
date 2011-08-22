0db45c21e348092e75b94bee487b37f84f5fcb8a
John Wilkinson <jcwilk@gmail.com>
2011-08-21 19:53:50 -0700

    Converting to unix linebreaks

--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ if ! test -f "$chef_binary"; then
     apt-get -o Dpkg::Options::="--force-confnew" \
         --force-yes -fuy dist-upgrade &&
     # Install Ruby and Chef
-    aptitude install -y ruby1.9.1 ruby1.9.1-dev make &&
-    sudo gem1.9.1 install --no-rdoc --no-ri chef --version 0.10.0
+    aptitude install -y ruby1.9.2 ruby1.9.2-dev make &&
+    sudo gem1.9.2 install --no-rdoc --no-ri chef --version 0.10.4
 fi &&
 
 "$chef_binary" -c solo.rb -j solo.json
