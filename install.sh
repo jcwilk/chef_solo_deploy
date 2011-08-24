10786df37eb4d7cd7d8de887c1c5a24160914e3e
John Wilkinson <jcwilk@gmail.com>
2011-08-24 00:59:21 -0700

    Undid my overzealous pursuit of 1.9.2, ubuntu isn't so zealous yet apparently

--- install.sh
+++ install.sh
@@ -8,12 +8,12 @@ chef_binary=/var/lib/gems/1.9.1/bin/chef-solo
 if ! test -f "$chef_binary"; then
     export DEBIAN_FRONTEND=noninteractive
     # Upgrade headlessly (this is only safe-ish on vanilla systems)
-    aptitude update &&
+    apt-get update &&
     apt-get -o Dpkg::Options::="--force-confnew" \
         --force-yes -fuy dist-upgrade &&
     # Install Ruby and Chef
-    aptitude install -y ruby1.9.2 ruby1.9.2-dev make &&
-    sudo gem1.9.2 install --no-rdoc --no-ri chef --version 0.10.4
+    apt-get install -y ruby1.9.1 ruby1.9.1-dev make &&
+    sudo gem1.9.1 install --no-rdoc --no-ri chef --version 0.10.4
 fi &&
 
 "$chef_binary" -c solo.rb -j solo.json
