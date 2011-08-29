41542b92b00c993f88823c079f25a046557ac2cb
John Wilkinson <jcwilk@gmail.com>
2011-08-28 19:57:36 -0700

    Broke the tf2 install step into steam and tf2
    Fixed some directory path issues (hopefully)

--- cookbooks/tf2/recipes/default.rb
+++ cookbooks/tf2/recipes/default.rb
@@ -28,26 +28,36 @@ directory srcds_root do
   mode "0755"
 end
 
-script "install tf2" do
+script "install steam" do
   interpreter "bash"
   user "chuck"
   cwd srcds_root
-  not_if "test -d #{File.join(srcds_root,'organgebox/tf')}"
+  not_if "test -d #{File.join(srcds_root,'/steam')}"
   code <<-EOH
   wget http://www.steampowered.com/download/hldsupdatetool.bin
   chmod +x hldsupdatetool.bin
   echo "yes" | ./hldsupdatetool.bin
   chmod +x steam
-  ./steam
-  ./steam -command update -game tf -dir . -verify_all
+  EOH
+end
+
+script "install tf2" do
+  interpreter "bash"
+  user "chuck"
+  cwd srcds_root
+  not_if "test -d #{File.join(srcds_root,'/organgebox/tf')}"
+  code <<-EOH
+  ./steam -command update -game tf -dir . -verify_all -retry
   EOH
 end
 
 #server.cfg originally from http://forums.srcds.com/viewtopic/5264
 file File.join(srcds_root, 'orangebox/tf/cfg/server.cfg') do
+  user "chuck"
   mode "644"
 end
 
 #The command is supposed to be something like the following, but I'm not seeing
 #any file like that presently...
 #nohup ./srcds_run -game tf +map ctf_2fort +maxplayers 12 -autoupdate &
+#going to use an init script instead
